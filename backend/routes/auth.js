184ee702beaf509f96657b1547a6fe0ee90f01f7
Tharun <gowritharun461@gmail.com>
2026-02-18 10:29:51 +0530

    imagebb

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import  jwt from 'jsonwebtoken';
-import Merchant from '../models/merchant.js';
+import Merchant from '../models/Merchant.js';
 
 const router = express.Router();
 
